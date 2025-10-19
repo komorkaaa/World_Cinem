620bfcf56dcbd43c9bb1784f487e8efe8334fb9d
komorkaaa <mr.komorka@gmail.com>
2025-10-19 15:55:59 +0300

    Добавил SplashFragment.kt

--- app/src/main/java/com/example/worldcinema/MainActivity.kt
+++ app/src/main/java/com/example/worldcinema/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.worldcinema
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_WorldCinema) //
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
--- app/src/main/java/com/example/worldcinema/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/worldcinema/ui/splash/SplashFragment.kt
@@ -1,2 +1,31 @@
 package com.example.worldcinema.ui.splash
 
+import com.example.worldcinema.databinding.FragmentSplashBinding
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class SplashFragment : Fragment() {
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+        FragmentSplashBinding.inflate(inflater, container, false).also { _binding = it }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(com.example.worldcinema.R.id.action_splash_to_login)
+        }, 2000)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
